6e982d5df63f9c5e769390393018684abb5251cf
Alikhan <fixator2004@gmail.com>
2025-01-26 18:45:19 -0500

    Task 1 - Store Inventory.

---
+++ cc_2.js
@@ -0,0 +1,10 @@
+//task 1 
+
+let products = ["Milk", "Bread", "Chicken", "Oil", "Eggs"]; //an array of 5 products
+products.push("Mayo"); //adds a new product
+products.pop(); //removes last product
+
+console.log(products);
+
+
+//task 2 
