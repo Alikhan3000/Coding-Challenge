a8492a04853cf6f6a301ee26090948390c3c5d7a
Alikhan <fixator2004@gmail.com>
2025-01-26 19:22:08 -0500

    Task 3 - Employee Records.

--- cc_2.js
+++ cc_2.js
@@ -8,3 +8,28 @@ console.log(products);
 
 
 //task 2 
+let scores = [94, 85, 77, 99, 65]; //array of 5 scores
+scores[1] = 90; //second score changed to 90
+let scores_total = scores.reduce((sum, score) => sum + score, 0);
+avg_scores = scores_total / scores.length; // sum of scores divided by the lenght of the array 
+
+console.log(avg_scores);
+
+//task 3 
+let employee = {
+
+    name: "Ramazan Gaparov",
+    age: 60,
+    department: "IT",
+    isActive: true
+
+};
+
+employee.department = "Customer Service"; //updating department to customer service
+employee.position = "Sales Representative";//new property added
+
+console.log(employee)
+
+
+//task 4 
+
